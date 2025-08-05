c44d539bdb00d43b08751066283161d4513d3a2c
Ampmusicsog <aaamo-mensah@st.ug.edu.gh>
2025-08-05 20:05:38 +0000

    AbstractClasses added

---
+++ AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AbstractClassesAndMethods
+
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5.0);
+            Shape rectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine($"Area of the Circle: {circle.GetArea()}");
+            Console.WriteLine($"Area of the Rectangle: {rectangle.GetArea()}");
+        }
+    }
+}
